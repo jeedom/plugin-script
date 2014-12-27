6ca4b05897464a93713144d351a384f18b56ac22
Loic <loicgevrey@gmail.com>
2014-12-27 21:50:14 +0100

    Bufix

--- core/class/script.class.php
+++ core/class/script.class.php
@@ -79,32 +79,32 @@ class script extends eqLogic {
         } else {
             switch ($res) {
                 case ZipArchive::ER_EXISTS:
-                    $ErrMsg = "Le fichier existe déjà.";
-                    break;
+                $ErrMsg = "Le fichier existe déjà.";
+                break;
                 case ZipArchive::ER_INCONS:
-                    $ErrMsg = "L'archive est inconsistante.";
-                    break;
+                $ErrMsg = "L'archive est inconsistante.";
+                break;
                 case ZipArchive::ER_MEMORY:
-                    $ErrMsg = "Echec d'allocation mémoire (malloc).";
-                    break;
+                $ErrMsg = "Echec d'allocation mémoire (malloc).";
+                break;
                 case ZipArchive::ER_NOENT:
-                    $ErrMsg = "Le fichier n'existe pas.";
-                    break;
+                $ErrMsg = "Le fichier n'existe pas.";
+                break;
                 case ZipArchive::ER_NOZIP:
-                    $ErrMsg = "Ce n'est pas une archive zip.";
-                    break;
+                $ErrMsg = "Ce n'est pas une archive zip.";
+                break;
                 case ZipArchive::ER_OPEN:
-                    $ErrMsg = "Le fichier ne peut pas être ouvert.";
-                    break;
+                $ErrMsg = "Le fichier ne peut pas être ouvert.";
+                break;
                 case ZipArchive::ER_READ:
-                    $ErrMsg = "Erreur de lecture.";
-                    break;
+                $ErrMsg = "Erreur de lecture.";
+                break;
                 case ZipArchive::ER_SEEK:
-                    $ErrMsg = "Erreur de recherche.";
-                    break;
+                $ErrMsg = "Erreur de recherche.";
+                break;
                 default:
-                    $ErrMsg = "Unknow (Code $res)";
-                    break;
+                $ErrMsg = "Unknow (Code $res)";
+                break;
             }
             throw new Exception(__('Impossible de décompresser l\'archive zip : ', __FILE__) . $_path . 'Erreur : ' . $ErrMsg);
         }
@@ -166,63 +166,76 @@ class scriptCmd extends cmd {
         if ($_options != null) {
             switch ($this->getType()) {
                 case 'action' :
-                    switch ($this->getSubType()) {
-                        case 'slider':
-                            $request = str_replace('#slider#', $_options['slider'], $request);
-                            break;
-                        case 'color':
-                            $request = str_replace('#color#', $_options['color'], $request);
-                            break;
-                        case 'message':
-                            $replace = array('#title#', '#message#');
-                            $replaceBy = array(urlencode($_options['title']), urlencode($_options['message']));
-                            if ($_options['message'] == '' || $_options['title'] == '') {
-                                throw new Exception(__('Le message et le sujet ne peuvent pas être vide', __FILE__));
-                            }
-                            $request = str_replace($replace, $replaceBy, $request);
-                            break;
-                    }
+                switch ($this->getSubType()) {
+                    case 'slider':
+                    $request = str_replace('#slider#', $_options['slider'], $request);
                     break;
+                    case 'color':
+                    $request = str_replace('#color#', $_options['color'], $request);
+                    break;
+                    case 'message':
+                    $replace = array('#title#', '#message#');
+                    $replaceBy = array(urlencode($_options['title']), urlencode($_options['message']));
+                    if ($_options['message'] == '' || $_options['title'] == '') {
+                        throw new Exception(__('Le message et le sujet ne peuvent pas être vide', __FILE__));
+                    }
+                    $request = str_replace($replace, $replaceBy, $request);
+                    break;
+                }
+                break;
             }
         }
         $request = scenarioExpression::setTags($request);
+        $request = str_replace('"','',$request);
 
         switch ($this->getConfiguration('requestType')) {
             case 'http' :
-                if ($this->getConfiguration('http_username') != '' && $this->getConfiguration('http_password') != '') {
-                    $request_http = new com_http($request, $this->getConfiguration('http_username'), $this->getConfiguration('http_password'));
-                } else {
-                    $request_http = new com_http($request);
-                }
-                if ($this->getConfiguration('allowEmptyResponse') == 1) {
-                    $request_http->setAllowEmptyReponse(true);
-                }
-                if ($this->getConfiguration('noSslCheck') == 1) {
-                    $request_http->setNoSslCheck(true);
-                }
-                if ($this->getConfiguration('doNotReportHttpError') == 1) {
-                    $request_http->setNoReportError(true);
-                }
-                $result = trim($request_http->exec($this->getConfiguration('timeout', 2), $this->getConfiguration('maxHttpRetry', 3)));
-                if ($this->getConfiguration('reponseMustContain') != '' && strpos($result, $this->getConfiguration('reponseMustContain')) !== false) {
-                    throw new Exception(__('La réponse ne contient pas : ', __FILE__) . $this->getConfiguration('reponseMustContain') . ' : ' . $result);
-                }
-                break;
+            if ($this->getConfiguration('http_username') != '' && $this->getConfiguration('http_password') != '') {
+                $request_http = new com_http($request, $this->getConfiguration('http_username'), $this->getConfiguration('http_password'));
+            } else {
+                $request_http = new com_http($request);
+            }
+            if ($this->getConfiguration('allowEmptyResponse') == 1) {
+                $request_http->setAllowEmptyReponse(true);
+            }
+            if ($this->getConfiguration('noSslCheck') == 1) {
+                $request_http->setNoSslCheck(true);
+            }
+            if ($this->getConfiguration('doNotReportHttpError') == 1) {
+                $request_http->setNoReportError(true);
+            }
+            $result = trim($request_http->exec($this->getConfiguration('timeout', 2), $this->getConfiguration('maxHttpRetry', 3)));
+            if ($this->getConfiguration('reponseMustContain') != '' && strpos($result, $this->getConfiguration('reponseMustContain')) !== false) {
+                throw new Exception(__('La réponse ne contient pas : ', __FILE__) . $this->getConfiguration('reponseMustContain') . ' : ' . $result);
+            }
+            break;
             case 'script' :
-                if (strpos($request, '.php') !== false) {
-                    $request_shell = new com_shell('php ' . $request . ' 2>&1');
-                } else if (strpos($request, '.rb') !== false) {
-                    $request_shell = new com_shell('ruby ' . $request . ' 2>&1');
-                } else if (strpos($request, '.py') !== false) {
-                    $request_shell = new com_shell('python ' . $request . ' 2>&1');
-                } else if (strpos($request, '.pl') !== false) {
-                    $request_shell = new com_shell('perl ' . $request . ' 2>&1');
-                } else {
-                    $request_shell = new com_shell($request . ' 2>&1');
-                }
-                $result = trim($request_shell->exec());
-                break;
+            if (strpos($request, '.php') !== false) {
+                $request_shell = new com_shell('php ' . $request . ' 2>&1');
+            } else if (strpos($request, '.rb') !== false) {
+                $request_shell = new com_shell('ruby ' . $request . ' 2>&1');
+            } else if (strpos($request, '.py') !== false) {
+                $request_shell = new com_shell('python ' . $request . ' 2>&1');
+            } else if (strpos($request, '.pl') !== false) {
+                $request_shell = new com_shell('perl ' . $request . ' 2>&1');
+            } else {
+                $request_shell = new com_shell($request . ' 2>&1');
+            }
+            $result = trim($request_shell->exec());
+            break;
             case 'xml' :
+            if ($this->getConfiguration('xml_username') != '' && $this->getConfiguration('xml_password') != '') {
+                $request_http = new com_http($this->getConfiguration('urlXml'), $this->getConfiguration('xml_username'), $this->getConfiguration('xml_password'));
+            } else {
+                $request_http = new com_http($this->getConfiguration('urlXml'));
+            }
+            if ($this->getConfiguration('xmlNoSslCheck') == 1) {
+                $request_http->setNoSslCheck(true);
+            }
+            $xml = trim($request_http->exec($this->getConfiguration('xmlTimeout', 2), $this->getConfiguration('maxXmlRetry', 3)));
+            try {
+                $xml = new SimpleXMLElement($xml);
+            } catch (Exception $e) {
                 if ($this->getConfiguration('xml_username') != '' && $this->getConfiguration('xml_password') != '') {
                     $request_http = new com_http($this->getConfiguration('urlXml'), $this->getConfiguration('xml_username'), $this->getConfiguration('xml_password'));
                 } else {
@@ -232,42 +245,42 @@ class scriptCmd extends cmd {
                     $request_http->setNoSslCheck(true);
                 }
                 $xml = trim($request_http->exec($this->getConfiguration('xmlTimeout', 2), $this->getConfiguration('maxXmlRetry', 3)));
-                try {
-                    $xml = new SimpleXMLElement($xml);
-                } catch (Exception $e) {
-                    if ($this->getConfiguration('xml_username') != '' && $this->getConfiguration('xml_password') != '') {
-                        $request_http = new com_http($this->getConfiguration('urlXml'), $this->getConfiguration('xml_username'), $this->getConfiguration('xml_password'));
-                    } else {
-                        $request_http = new com_http($this->getConfiguration('urlXml'));
-                    }
-                    if ($this->getConfiguration('xmlNoSslCheck') == 1) {
-                        $request_http->setNoSslCheck(true);
-                    }
-                    $xml = trim($request_http->exec($this->getConfiguration('xmlTimeout', 2), $this->getConfiguration('maxXmlRetry', 3)));
-                    $xml = new SimpleXMLElement($xml);
-                }
-                $json = json_decode(json_encode($xml), TRUE);
-                $tags = explode('>', $request);
-                foreach ($tags as $tag) {
-                    $tag = trim($tag);
-                    if (isset($json[$tag])) {
-                        $json = $json[$tag];
-                    } else if (is_numeric(intval($tag)) && isset($json[intval($tag)])) {
-                        $json = $json[intval($tag)];
-                    } else {
-                        $json = '';
-                        break;
-                    }
-                }
-                if (is_array($json)) {
-                    $result = '';
+                $xml = new SimpleXMLElement($xml);
+            }
+            $json = json_decode(json_encode($xml), TRUE);
+            $tags = explode('>', $request);
+            foreach ($tags as $tag) {
+                $tag = trim($tag);
+                if (isset($json[$tag])) {
+                    $json = $json[$tag];
+                } else if (is_numeric(intval($tag)) && isset($json[intval($tag)])) {
+                    $json = $json[intval($tag)];
                 } else {
-                    $result = $json;
+                    $json = '';
+                    break;
                 }
-                return $result;
+            }
+            if (is_array($json)) {
+                $result = '';
+            } else {
+                $result = $json;
+            }
+            return $result;
             case 'json' :
-                if ($this->getConfiguration('json_username') != '' && $this->getConfiguration('json_password') != '') {
-                    $request_http = new com_http($this->getConfiguration('urlJson'), $this->getConfiguration('json_username'), $this->getConfiguration('json_password'));
+            if ($this->getConfiguration('json_username') != '' && $this->getConfiguration('json_password') != '') {
+                $request_http = new com_http($this->getConfiguration('urlJson'), $this->getConfiguration('json_username'), $this->getConfiguration('json_password'));
+            } else {
+                $request_http = new com_http($this->getConfiguration('urlJson'));
+            }
+            if ($this->getConfiguration('jsonNoSslCheck') == 1) {
+                $request_http->setNoSslCheck(true);
+            }
+            $json = trim($request_http->exec($this->getConfiguration('jsonTimeout', 2), $this->getConfiguration('maxJsonRetry', 3)));
+            try {
+                $json = json_decode($json, true);
+            } catch (Exception $e) {
+                if ($this->getConfiguration('json_username') != '' && $this->getConfiguration('json_username') != '') {
+                    $request_http = new com_http($this->getConfiguration('urlJson'), $this->getConfiguration('json_username'), $this->getConfiguration('json_username'));
                 } else {
                     $request_http = new com_http($this->getConfiguration('urlJson'));
                 }
@@ -275,38 +288,26 @@ class scriptCmd extends cmd {
                     $request_http->setNoSslCheck(true);
                 }
                 $json = trim($request_http->exec($this->getConfiguration('jsonTimeout', 2), $this->getConfiguration('maxJsonRetry', 3)));
-                try {
-                    $json = json_decode($json, true);
-                } catch (Exception $e) {
-                    if ($this->getConfiguration('json_username') != '' && $this->getConfiguration('json_username') != '') {
-                        $request_http = new com_http($this->getConfiguration('urlJson'), $this->getConfiguration('json_username'), $this->getConfiguration('json_username'));
-                    } else {
-                        $request_http = new com_http($this->getConfiguration('urlJson'));
-                    }
-                    if ($this->getConfiguration('jsonNoSslCheck') == 1) {
-                        $request_http->setNoSslCheck(true);
-                    }
-                    $json = trim($request_http->exec($this->getConfiguration('jsonTimeout', 2), $this->getConfiguration('maxJsonRetry', 3)));
-                    $json = json_decode($json, true);
-                }
-                $tags = explode('>', $request);
-                foreach ($tags as $tag) {
-                    $tag = trim($tag);
-                    if (isset($json[$tag])) {
-                        $json = $json[$tag];
-                    } else if (is_numeric(intval($tag)) && isset($json[intval($tag)])) {
-                        $json = $json[intval($tag)];
-                    } else {
-                        $json = '';
-                        break;
-                    }
-                }
-                if (is_array($json)) {
-                    $result = '';
+                $json = json_decode($json, true);
+            }
+            $tags = explode('>', $request);
+            foreach ($tags as $tag) {
+                $tag = trim($tag);
+                if (isset($json[$tag])) {
+                    $json = $json[$tag];
+                } else if (is_numeric(intval($tag)) && isset($json[intval($tag)])) {
+                    $json = $json[intval($tag)];
                 } else {
-                    $result = $json;
+                    $json = '';
+                    break;
                 }
-                return $result;
+            }
+            if (is_array($json)) {
+                $result = '';
+            } else {
+                $result = $json;
+            }
+            return $result;
         }
         if ($this->getType() == 'action') {
             sleep(1);
