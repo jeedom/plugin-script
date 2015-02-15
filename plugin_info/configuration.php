4b8e55382ce00d82b450073c0ebb35b893f51499
Loic <loicgevrey@gmail.com>
2015-02-15 13:11:01 +0100

    Add translation

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -28,7 +28,7 @@ if (!isConnect()) {
 <form class="form-horizontal">
     <fieldset>
         <div class="form-group">
-            <label class="col-lg-2 control-label">Chemin des scripts utilisateur</label>
+            <label class="col-lg-2 control-label">{{Chemin des scripts utilisateur}}</label>
             <div class="col-lg-4">
                <input type="text" class="configKey form-control" data-l1key="userScriptDir" />
             </div>
