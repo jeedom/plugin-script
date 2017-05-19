30ad7f4796ea67bb6260b7a68a58698fd917edfa
Loïc <loic@jeedom.com>
2017-05-19 11:47:09 +0200

    Update script.class.php

--- core/class/script.class.php
+++ core/class/script.class.php
@@ -18,6 +18,7 @@
 
 /* * ***************************Includes********************************* */
 require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+require_once dirname(__FILE__) . '/../../vendor/autoload.php';
 
 class script extends eqLogic {
 
