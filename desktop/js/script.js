1554bde11196bef19330ad8322e5c84c8df2af9e
KiboOst <KiboOst@users.noreply.github.com>
2019-04-21 15:15:31 +0200

    update icon

--- desktop/js/script.js
+++ desktop/js/script.js
@@ -318,7 +318,7 @@
     tr += '<a class="btn btn-default btn-xs cmdAction" data-action="configure"><i class="fas fa-cogs"></i></a> ';
     tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fas fa-rss"></i> {{Tester}}</a>';
 }
-tr += ' <a class="btn btn-default btn-xs cmdAction" data-action="copy" title="Dupliquer"><i class="far fa-files"></i></a> ';
+tr += ' <a class="btn btn-default btn-xs cmdAction" data-action="copy" title="Dupliquer"><i class="far fa-clone"></i></a> ';
 tr += '<i class="fas fa-minus-circle pull-right cmdAction cursor" data-action="remove"></i></td>';
 tr += '</tr>';
 
