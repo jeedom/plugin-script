9499d2d7e0cce0d4790294497ddb663b7d10cce9
JEALG <jalain.gosse@icloud.com>
2019-12-24 12:34:43 +0100

    migration font-awesome 5
    Suite au sujet
    https://community.jeedom.com/t/icone-certains-icones-nont-pas-ete-migrer-sur-la-font-awesome-5/11852

--- desktop/js/script.js
+++ desktop/js/script.js
@@ -227,7 +227,7 @@
     tr += '<input class="cmdAttr form-control input-sm" data-l1key="id"  style="display : none;">';
     tr += '<div class="row">';
     tr += '<div class="col-sm-6">';
-    tr += '<a class="cmdAction btn btn-default btn-sm" data-l1key="chooseIcon"><i class="fa fa-flag"></i> Icone</a>';
+    tr += '<a class="cmdAction btn btn-default btn-sm" data-l1key="chooseIcon"><i class="fas fa-flag"></i> Icone</a>';
     tr += '<span class="cmdAttr" data-l1key="display" data-l2key="icon" style="margin-left : 10px;"></span>';
     tr += '</div>';
     tr += '<div class="col-sm-6">';
