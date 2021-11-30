c0b1159303f05d46977827ebc28121b78efe18ee
Loïc <loic@jeedom.com>
2021-11-30 12:02:10 +0100

    Add files via upload

--- desktop/php/script.php
+++ desktop/php/script.php
@@ -7,36 +7,6 @@ sendVarToJS('eqType', $plugin->getId());
 $eqLogics = eqLogic::byType($plugin->getId());
 $foldOnStart = config::byKey('foldOnStart', 'script', '0');
 sendVarToJS('foldOnStart', $foldOnStart);
-
-include_file('3rdparty', 'jquery.fileTree/jqueryFileTree', 'css', 'script');
-include_file('3rdparty', 'codemirror/lib/codemirror', 'js');
-include_file('3rdparty', 'codemirror/lib/codemirror', 'css');
-
-include_file('3rdparty', 'codemirror/mode/htmlmixed/htmlmixed', 'js');
-include_file('3rdparty', 'codemirror/mode/clike/clike', 'js');
-include_file('3rdparty', 'codemirror/mode/php/php', 'js');
-include_file('3rdparty', 'codemirror/mode/shell/shell', 'js');
-include_file('3rdparty', 'codemirror/mode/python/python', 'js');
-include_file('3rdparty', 'codemirror/mode/ruby/ruby', 'js');
-include_file('3rdparty', 'codemirror/mode/perl/perl', 'js');
-
-//Core CodeMirror addons:
-include_file('3rdparty', 'codemirror/addon/edit/matchbrackets', 'js');
-include_file('3rdparty', 'codemirror/addon/selection/active-line', 'js');
-include_file('3rdparty', 'codemirror/addon/search/search', 'js');
-include_file('3rdparty', 'codemirror/addon/search/searchcursor', 'js');
-include_file('3rdparty', 'codemirror/addon/dialog/dialog', 'js');
-include_file('3rdparty', 'codemirror/addon/dialog/dialog', 'css');
-
-include_file('3rdparty', 'codemirror/addon/fold/brace-fold', 'js');
-include_file('3rdparty', 'codemirror/addon/fold/comment-fold', 'js');
-include_file('3rdparty', 'codemirror/addon/fold/foldcode', 'js');
-include_file('3rdparty', 'codemirror/addon/fold/indent-fold', 'js');
-include_file('3rdparty', 'codemirror/addon/fold/markdown-fold', 'js');
-include_file('3rdparty', 'codemirror/addon/fold/xml-fold', 'js');
-include_file('3rdparty', 'codemirror/addon/fold/foldgutter', 'js');
-include_file('3rdparty', 'codemirror/addon/fold/foldgutter', 'css');
-
 sendVarToJS('userScriptDir', getRootPath() . '/' . config::byKey('userScriptDir', 'script'));
 ?>
 <style>
@@ -59,17 +29,40 @@ sendVarToJS('userScriptDir', getRootPath() . '/' . config::byKey('userScriptDir'
 			</div>
 		</div>
 		<legend><i class="fas fa-file"></i> {{Mes Scripts}}</legend>
-		<input class="form-control" placeholder="{{Rechercher}}" id="in_searchEqlogic" />
+
+		<div class="input-group" style="margin:5px;">
+		<input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic"/>
+		<div class="input-group-btn">
+		<a id="bt_resetSearch" class="btn" style="width:30px"><i class="fas fa-times"></i></a>
+		<a class="btn roundedRight hidden" id="bt_pluginDisplayAsTable" data-coreSupport="1" data-state="0"><i class="fas fa-grip-lines"></i></a>
+		</div>
+		</div>
+
 		<div class="eqLogicThumbnailContainer">
 			<?php
+			$content = '';
 			foreach ($eqLogics as $eqLogic) {
 				$opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
-				echo '<div class="eqLogicDisplayCard cursor '.$opacity.'" data-eqLogic_id="' . $eqLogic->getId() . '" >';
-				echo '<img src="' . $plugin->getPathImgIcon() . '" />';
-				echo '<br>';
-				echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
-				echo '</div>';
+				$eqString = '';
+				$eqString .= '<div class="eqLogicDisplayCard cursor '.$opacity.'" data-eqLogic_id="' . $eqLogic->getId() . '">';
+				$eqString .= '<img src="' . $plugin->getPathImgIcon() . '" />';
+				$eqString .= '<br>';
+				$eqString .= '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
+
+				$eqString .= '<span class="hidden hiddenAsCard displayTableRight">';
+				if ($eqLogic->getConfiguration('autorefresh') != '') $eqString .= '<span>' . $eqLogic->getConfiguration('autorefresh') . '</span>';
+				$cats = $eqLogic->getCategory();
+				unset($cats['default']);
+				$eqString .= '<span>' . implode(array_keys($cats, 1), ', ') . '</span>';
+				if ($eqLogic->getIsVisible() == 1) {
+					$eqString .= ' <i class="fas fa-eye"></i>';
+				} else {
+					$eqString .= ' <i class="fas fa-eye-slash"></i>';
+				}
+				$eqString .= '</span>';
+				$content .= $eqString . '</div>';
 			}
+			echo $content;
 			?>
 		</div>
 	</div>
@@ -182,17 +175,49 @@ sendVarToJS('userScriptDir', getRootPath() . '/' . config::byKey('userScriptDir'
 		</div>
 	</div>
 </div>
-<div id="md_browseScriptFile" title="Parcourir...">
+
+<div id="md_browseScriptFile" class="hidden" title="Parcourir...">
 	<div style="display: none;" id="div_browseScriptFileAlert"></div>
 	<div id="div_browseScriptFileTree"></div>
 </div>
 
-<div id="md_editScriptFile" title="Editer...">
+<div id="md_editScriptFile" class="hidden" title="Editer...">
 	<div style="display: none;" id="div_editScriptFileAlert"></div>
 	<textarea id="ta_editScriptFile" class="form-control" style="height: 100%;"></textarea>
 </div>
 
-<?php include_file('3rdparty', 'jquery.fileTree/jquery.easing.1.3', 'js', 'script');?>
-<?php include_file('3rdparty', 'jquery.fileTree/jqueryFileTree', 'js', 'script');?>
-<?php include_file('desktop', 'script', 'js', 'script');?>
-<?php include_file('core', 'plugin.template', 'js');?>
+<?php
+	include_file('3rdparty', 'jquery.fileTree/jqueryFileTree', 'css', 'script');
+	include_file('3rdparty', 'codemirror/lib/codemirror', 'js');
+	include_file('3rdparty', 'codemirror/lib/codemirror', 'css');
+
+	include_file('3rdparty', 'codemirror/mode/htmlmixed/htmlmixed', 'js');
+	include_file('3rdparty', 'codemirror/mode/clike/clike', 'js');
+	include_file('3rdparty', 'codemirror/mode/php/php', 'js');
+	include_file('3rdparty', 'codemirror/mode/shell/shell', 'js');
+	include_file('3rdparty', 'codemirror/mode/python/python', 'js');
+	include_file('3rdparty', 'codemirror/mode/ruby/ruby', 'js');
+	include_file('3rdparty', 'codemirror/mode/perl/perl', 'js');
+
+	//Core CodeMirror addons:
+	include_file('3rdparty', 'codemirror/addon/edit/matchbrackets', 'js');
+	include_file('3rdparty', 'codemirror/addon/selection/active-line', 'js');
+	include_file('3rdparty', 'codemirror/addon/search/search', 'js');
+	include_file('3rdparty', 'codemirror/addon/search/searchcursor', 'js');
+	include_file('3rdparty', 'codemirror/addon/dialog/dialog', 'js');
+	include_file('3rdparty', 'codemirror/addon/dialog/dialog', 'css');
+
+	include_file('3rdparty', 'codemirror/addon/fold/brace-fold', 'js');
+	include_file('3rdparty', 'codemirror/addon/fold/comment-fold', 'js');
+	include_file('3rdparty', 'codemirror/addon/fold/foldcode', 'js');
+	include_file('3rdparty', 'codemirror/addon/fold/indent-fold', 'js');
+	include_file('3rdparty', 'codemirror/addon/fold/markdown-fold', 'js');
+	include_file('3rdparty', 'codemirror/addon/fold/xml-fold', 'js');
+	include_file('3rdparty', 'codemirror/addon/fold/foldgutter', 'js');
+	include_file('3rdparty', 'codemirror/addon/fold/foldgutter', 'css');
+
+	include_file('3rdparty', 'jquery.fileTree/jquery.easing.1.3', 'js', 'script');
+	include_file('3rdparty', 'jquery.fileTree/jqueryFileTree', 'js', 'script');
+	include_file('desktop', 'script', 'js', 'script');
+	include_file('core', 'plugin.template', 'js');
+?>
