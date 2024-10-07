629f709048919c3b684cb630faad7a335bd2c4a5
Loïc <1536036+zoic21@users.noreply.github.com>
2024-10-07 09:55:48 +0200

    cleaning plugin

--- core/class/script.class.php
+++ core/class/script.class.php
@@ -18,7 +18,9 @@
 
 /* * ***************************Includes********************************* */
 require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
-require_once dirname(__FILE__) . '/../../vendor/autoload.php';
+if(file_exists(dirname(__FILE__) . '/../../vendor/autoload.php')){
+	require_once dirname(__FILE__) . '/../../vendor/autoload.php';
+}
 
 class script extends eqLogic {
 
